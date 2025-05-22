a5ddb1cfcd8a040461ceae313bf6e610ca8d1d32
i-budonnyi <polandgdansk2020@gmail.com>
2025-05-22 10:42:43 +0300

    ihjoiuh

--- src/components/Admin/AdminDashboard.js
+++ src/components/Admin/AdminDashboard.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const API_BASE = "https://backend-avtologistika.onrender.com/api/admin";
+const API_BASE = "https://backend-avtologistika.onrender.com/api/administratorsRoutes";
 
 const AdminDashboard = () => {
   const navigate = useNavigate();
