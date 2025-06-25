7a24ceba63070a8949e5725d7821de173f021376
i-budonnyi <polandgdansk2020@gmail.com>
2025-06-25 15:29:09 +0300

    NGFJH

--- src/components/Subscriptions/Subscriptions.js
+++ src/components/Subscriptions/Subscriptions.js
@@ -22,7 +22,7 @@ const { Option } = Select;
 
 const API_BASE = "https://backend-avtologistika.onrender.com/api";
 const API_SUBSCRIPTIONS_URL = `${API_BASE}/subscriptionRoutes/user-subscriptions`;
-const API_STATUSES_URL = `${API_BASE}/statusRoutes/get-statuses`; // ✅ виправлено
+const API_STATUSES_URL = `${API_BASE}/statusRoutes/get-statuses`; // ✅ випрлено
 
 const Subscriptions = () => {
   const [subscriptions, setSubscriptions] = useState([]);
